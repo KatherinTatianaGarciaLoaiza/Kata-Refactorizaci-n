86c8f336d999cb62b9d7727452ed39562aa0c2e5
unknown <ktatiana.garcia@udea.edu.co>
2021-03-27 23:25:39 -0500

    Kata.TennisGame1 con pequeños comentarios

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,16 +1,20 @@
-
+/*Este es un codigo para un juegos de tennis con dificultad 1.
+ *Ha sido refactorizado por Katherin Tatiana Garcia Loaiza con fin de practicar la refactorizacion.
+ */
 public class TennisGame1 implements TennisGame {
-    
+
     private int ScorePlayer1 = 0;
     private int ScorePlayer2 = 0;
     private String Player1Name;
     private String Player2Name;
 
+    //Asignacion de nombres de los jugadores
     public TennisGame1(String Player1Name, String Player2Name) {
         this.Player1Name = Player1Name;
         this.Player2Name = Player2Name;
     }
 
+    //Asignacion del incremento de puntaje.
     public void wonPoint(String PlayerName) {
         if (PlayerName == "player1") {
             ScorePlayer1 += 1;
@@ -20,6 +24,7 @@ public class TennisGame1 implements TennisGame {
         }
     }
 
+    //Seleccion de opcion: empate, aumento de puntaje o ganador.
     public String getScore() {
         String Score = "";
         if (ScorePlayer1 == ScorePlayer2){
@@ -33,6 +38,7 @@ public class TennisGame1 implements TennisGame {
         return Score;
     }
 
+    //Opcion empate.
     public String PuntajeDeEmpate(int ScorePlayer){
         switch (ScorePlayer)
         {
@@ -47,6 +53,7 @@ public class TennisGame1 implements TennisGame {
         }
     }
 
+    //opcion de ganador o jugador que avanza.
     public String Avance(int ScorePlayer1, int ScorePlayer2){
         String score = "";
         int DiferenciaDePuntaje = ScorePlayer1 - ScorePlayer2;
@@ -65,6 +72,7 @@ public class TennisGame1 implements TennisGame {
         return score;
     }
 
+    //Opcion de incremento de puntaje.
     public String Puntaje(int ScorePlayer) {
         switch (ScorePlayer) {
             case 0:
